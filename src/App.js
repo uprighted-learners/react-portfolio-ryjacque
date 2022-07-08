664c0bfb7c59e066b36546fbce9bc72f0862fee4
Ryan Collins <ryanjcollins41@gmail.com>
2022-07-08 14:53:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="page-wrapper">
+    <nav className="nav-bar">
+      <Link to="/">HOME</Link>
+      <Link to="/about">ABOUT</Link>
+      </nav>
+    <div className="content-wrapper">
+    <div className="header"><h1>Ryan J Collins</h1><h2>Software Development Portfolio</h2></div>
+  
+    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    
+
+    <footer><Link to="/contact">Contact</Link><div>LinkedIn I guess</div></footer>
+    </div>  
+    <div id="project-div">
+      <Link  to="/projects"><h1>PROJECTS</h1></Link>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+function About() {
+ 
+  checkPath()
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
