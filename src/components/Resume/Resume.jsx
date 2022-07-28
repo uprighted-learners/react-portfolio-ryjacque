40f98f8d56f7d0097f0449d8a7d9e37e25f3ef5e
Ryan Collins <ryanjcollins41@gmail.com>
2022-07-28 10:23:29 -0400

    rendered resume as pdf in an iframe, rather than img

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./Resume.css";
-import resume from "./assets/resume.png"
+import resume from "./assets/resume.pdf"
 
 function Resume(props) {
   props.setIsProj(false)
+  
   return (
     <div id="resume-div">
-      <img
-        id="resume"
-        src={resume}
-        alt="resume pdf"
-      />
+      <iframe src={resume} id='resume' />
       <a
         href="https://drive.google.com/file/d/1nPYHfrPYC_X5mxMtuNkk7ajRiERj1jNq/view?usp=sharing"
         target="_blank"
